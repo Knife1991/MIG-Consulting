f90489c27fcf0367148a8531c90e25b038b3d0b8
Merola Giuseppe <merolagiuseppe91@outlook.it>
2024-09-25 20:14:21 +0200

    sistemati link nav e footer da tablet

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -2,12 +2,12 @@
     <div class="container px-6">
         <div class="alert bg_cookie rounded-lg">
             <div class="d-flex justify-content-between align-items-center">
-                <div class="max-w-full flex-1">
+                <div class="max-w-full col-7 col-md-8 col-lg-10">
                     <p class="text-dark cookie-consent__message mt-3">
                         {!! trans('cookie-consent::texts.message') !!}
                     </p>
                 </div>
-                <div class="mt-1 w-auto">
+                <div class="mt-1 w-auto col-3 col-md-4 col-lg-2">
                     <button class="js-cookie-consent-agree cookie-consent__agree btn ">
                         {{ trans('cookie-consent::texts.agree') }}
                     </button>
