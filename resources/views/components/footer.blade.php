4baa0b067fc8a320fc246f4ac4a56012c974e34c
Merola Giuseppe <merolagiuseppe91@outlook.it>
2025-10-10 11:19:25 +0200

    anche nel footer tolgo sezione momentanea

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -19,7 +19,7 @@
         <li class="nav-item mb-2"><a href="{{route('formazioneOperativa')}}" class="nav-link p-0 text-body-secondary">Formazione Operativa</a></li>
         <li class="nav-item mb-2"><a href="{{route('formazioneRuolo')}}" class="nav-link p-0 text-body-secondary">Formazione di ruolo</a></li>
         <li class="nav-item mb-2"><a href="{{route('consulenza-aziendale')}}" class="nav-link p-0 text-body-secondary">Consulenza Aziendale</a></li>
-        <li class="nav-item mb-2"><a href="{{route('blog.index')}}" class="nav-link p-0 text-body-secondary">Blog</a></li>
+        {{-- <li class="nav-item mb-2"><a href="{{route('blog.index')}}" class="nav-link p-0 text-body-secondary">Blog</a></li> --}}
         <li class="nav-item mb-2"><a href="{{route('contatti')}}" class="nav-link p-0 text-body-secondary">Contatti</a></li>
         <li class="nav-item mb-2"><a href="{{route('privacy')}}" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
       </ul>
